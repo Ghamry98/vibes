195d285726d5b8111319efd9ffdd3f7c4ec23862
ghamry98 <ghamry98@hotmail.com>
2021-02-16 14:10:33 +0200

    adding theme colors
    Signed-off-by: ghamry98 <ghamry98@hotmail.com>

---
+++ lib/config/ThemeColors.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  // Common colors
+  static Color _redColor = Color(0xFFE51515);
+  static Color _blueColor = Color(0xFF1E7AD1);
+  static Color _greenColor = Color(0xFF41BE9D);
+  static Color _yellowColor = Color(0xFFF6BD03);
+
+  static Color _basicColor = Color(0xFF575A5F);
+  static Color _successColor = Color(0xFF2DAA78);
+  static Color _warningColor = Color(0xFFFAB408);
+  static Color _errorColor = Color(0xFFF8626C);
+  static Color _trendingColor = Color(0xFF80EDC1);
+
+  // Light colors
+  static Color _lightRedColor = Color(0xFFEF7E6E);
+  static Color _lightBlueColor = Color(0xFF02A5F2);
+  static Color _lightGreenColor = Color(0xFF5BE0BD);
+  static Color _lightYellowColor = Color(0xFFFFF8D3);
+
+  static Color _primaryColor = Color(0xFFFFFFFF);
+  static Color _scaffoldColor = Color(0xFFFAF9FF);
+  static Color _focusColor = Color(0xFF606266);
+  static Color _inactiveColor = Color(0xFFE1E2E5);
+  static Color _dividerColor = Color(0xFFE7E7E7);
+  static Color _textPrimaryColor = Color(0xFF12121C);
+  static Color _textSecondaryColor = Color(0xFF84888D);
+
+  // Dark colors
+  static Color _darkRedColor = Color(0xFFEF7E6E);
+  static Color _darkBlueColor = Color(0xFF02A5F2);
+  static Color _darkGreenColor = Color(0xFF5BE0BD);
+  static Color _darkYellowColor = Color(0xFFFFF8D3);
+
+  static Color _primaryDarkColor = Color(0xFF1E1E1E);
+  static Color _scaffoldDarkColor = Color(0xFF181818);
+  static Color _focusDarkColor = Color(0xFF606266);
+  static Color _inactiveDarkColor = Color(0xFF212121);
+  static Color _dividerDarkColor = Color(0xFF626677);
+  static Color _textPrimaryDarkColor = Color(0xFFFFFFFF);
+  static Color _textSecondaryDarkColor = Color(0xFFAAAAAA);
+
+  // Methods
+  static Color redColor(double opacity) {
+    return _redColor.withOpacity(opacity);
+  }
+
+  static Color blueColor(double opacity) {
+    return _blueColor.withOpacity(opacity);
+  }
+
+  static Color greenColor(double opacity) {
+    return _greenColor.withOpacity(opacity);
+  }
+
+  static Color yellowColor(double opacity) {
+    return _yellowColor.withOpacity(opacity);
+  }
+
+  static Color basicColor(double opacity) {
+    return _basicColor.withOpacity(opacity);
+  }
+
+  static Color successColor(double opacity) {
+    return _successColor.withOpacity(opacity);
+  }
+
+  static Color warningColor(double opacity) {
+    return _warningColor.withOpacity(opacity);
+  }
+
+  static Color errorColor(double opacity) {
+    return _errorColor.withOpacity(opacity);
+  }
+
+  static Color trendingColor(double opacity) {
+    return _trendingColor.withOpacity(opacity);
+  }
+
+  static Color lightRedColor(double opacity) {
+    return _lightRedColor.withOpacity(opacity);
+  }
+
+  static Color lightBlueColor(double opacity) {
+    return _lightBlueColor.withOpacity(opacity);
+  }
+
+  static Color lightGreenColor(double opacity) {
+    return _lightGreenColor.withOpacity(opacity);
+  }
+
+  static Color lightYellowColor(double opacity) {
+    return _lightYellowColor.withOpacity(opacity);
+  }
+
+  static Color primaryColor(double opacity) {
+    return _primaryColor.withOpacity(opacity);
+  }
+
+  static Color scaffoldColor(double opacity) {
+    return _scaffoldColor.withOpacity(opacity);
+  }
+
+  static Color focusColor(double opacity) {
+    return _focusColor.withOpacity(opacity);
+  }
+
+  static Color inactiveColor(double opacity) {
+    return _inactiveColor.withOpacity(opacity);
+  }
+
+  static Color dividerColor(double opacity) {
+    return _dividerColor.withOpacity(opacity);
+  }
+
+  static Color textPrimaryColor(double opacity) {
+    return _textPrimaryColor.withOpacity(opacity);
+  }
+
+  static Color textSecondaryColor(double opacity) {
+    return _textSecondaryColor.withOpacity(opacity);
+  }
+
+  static Color darkRedColor(double opacity) {
+    return _darkRedColor.withOpacity(opacity);
+  }
+
+  static Color darkBlueColor(double opacity) {
+    return _darkBlueColor.withOpacity(opacity);
+  }
+
+  static Color darkGreenColor(double opacity) {
+    return _darkGreenColor.withOpacity(opacity);
+  }
+
+  static Color darkYellowColor(double opacity) {
+    return _darkYellowColor.withOpacity(opacity);
+  }
+
+  static Color primaryDarkColor(double opacity) {
+    return _primaryDarkColor.withOpacity(opacity);
+  }
+
+  static Color scaffoldDarkColor(double opacity) {
+    return _scaffoldDarkColor.withOpacity(opacity);
+  }
+
+  static Color focusDarkColor(double opacity) {
+    return _focusDarkColor.withOpacity(opacity);
+  }
+
+  static Color inactiveDarkColor(double opacity) {
+    return _inactiveDarkColor.withOpacity(opacity);
+  }
+
+  static Color dividerDarkColor(double opacity) {
+    return _dividerDarkColor.withOpacity(opacity);
+  }
+
+  static Color textPrimaryDarkColor(double opacity) {
+    return _textPrimaryDarkColor.withOpacity(opacity);
+  }
+
+  static Color textSecondaryDarkColor(double opacity) {
+    return _textSecondaryDarkColor.withOpacity(opacity);
+  }
+}
