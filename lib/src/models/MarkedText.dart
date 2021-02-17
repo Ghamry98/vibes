05e0ba2dd12e87a31699b5550b8aa74568b85132
ghamry98 <ghamry98@hotmail.com>
2021-02-17 12:24:26 +0200

    adding MarkedText model
    Signed-off-by: ghamry98 <ghamry98@hotmail.com>

---
+++ lib/src/models/MarkedText.dart
@@ -0,0 +1,17 @@
+enum TextMark { text, url, tag }
+
+class MarkedText {
+  // Attributes
+  String _text;
+  TextMark _type;
+
+  // Constructor
+  MarkedText(
+    this._text,
+    this._type,
+  );
+
+  // Methods
+  String get text => _text;
+  TextMark get type => _type;
+}
